44f8835fcbbaa7014722714d5630351a782a34ab
adkapse <apurvakapse97@gmail.com>
2019-07-29 13:12:24 +0530

    first commit

---
+++ src/Calc.java
@@ -0,0 +1,21 @@
+
+public class Calc {
+
+	public int add(int x,int y) {
+		return x+y;
+	}
+	public int subtract(int x,int y) {
+		return x-y;
+	}
+	public int mul(int x,int y) {
+		return x*y;
+	}
+	public int divide(int x,int y) {
+		return x/y;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
